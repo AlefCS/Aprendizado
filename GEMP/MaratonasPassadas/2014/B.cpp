b6bfa4f9b154f9487a98560044ce4bece499d607
Alef Carneiro de Sousa <carneiroalef@gmail.com>
2017-08-31 23:13:57 -0300

    Problema B devidamente resolvido!

--- GEMP/MaratonasPassadas/2014/B.cpp
+++ GEMP/MaratonasPassadas/2014/B.cpp
@@ -2,44 +2,19 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  int n, cont = 0;
-  //int *x, *xi, *xf;
-  int x[200000], xi[100000], xf[100000];
-  bool continuar = true;
+  int p, pos = 1, cont = 0;
 
-  scanf("%d", &n);
-
-  // x = (int*) malloc(n*8);
-  // xi = (int*) malloc(n*4);
-  // xf = (int*) malloc(n*4);
-
-  for (int i = 0; i < 2*n; i+=2) {
-    x[i] = i + 1;
-    x[i + 1] = i + 2;
-  }
-
-  while (continuar) {
+  scanf("%d", &p);
+  do {
     cont++;
-    for (int i = 0; i < n; i++) {
-      xi[i] = x[i];
-      xf[i] = x[i + n];
+    if (pos <= p/2) {
+      pos = 2*pos;
+    } else {
+      pos = 2*pos - p - 1;
     }
+  } while (pos != 1);
 
-    for (int i = 0; i < 2*n; i += 2) {
-      x[i] = xf[i/2];
-      x[i + 1] = xi[i/2];
-    }
-
-    continuar = false;
-
-    for (int i = 1; i < 2*n; i++) {
-      if (x[i - 1] > x[i]) {
-        continuar = true;
-      }
-    }
-  }
-
-  printf("Foram: %d\n", cont);
+  printf("%d\n", cont);
 
   return 0;
 }
